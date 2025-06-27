1b3797b5f2dd9f1b039f551732da561265e97007
VidhitSikri <sikrividhit@gmail.com>
2025-06-27 18:09:50 +0530

    feat(controller): implement validation and error handling in getAllExpenses method

--- server/controllers/expenses.controller.js
+++ server/controllers/expenses.controller.js
@@ -2,7 +2,16 @@ const expenseModel = require('../models/expenses.model');
 const { validationResult } = require('express-validator');
 
 module.exports.getAllExpenses = async (req, res) => {
-
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const expenses = await expenseModel.find();
+        res.status(200).json(expenses); // Send all expenses directly
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 
