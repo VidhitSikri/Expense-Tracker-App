7377ebc2333f95f61c7afd32b49aaeeb699fe54d
VidhitSikri <sikrividhit@gmail.com>
2025-06-27 17:32:49 +0530

    feat(server): set up Express server and MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-
+const dotenv = require("dotenv").config();
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-}   );  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const PORT = process.env.PORT;
+
+
+const cors = require("cors");
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+
+const connectToDb = require("./db/db");
+connectToDb();
+
+app.get("/", (req, res) => {   
+    res.send("Hello World!");
+}); 
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
