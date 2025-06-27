cbc84303634725f3c7b8e99490512a8f56b4d3c8
VidhitSikri <sikrividhit@gmail.com>
2025-06-27 17:37:35 +0530

    feat(models): create expense model with schema for title, amount, date, and description

---
+++ server/models/expenses.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    description: {
+        type: String,
+    }
+});
+
+const expenseModel = mongoose.model('expense', expenseSchema);
+
+module.exports = expenseModel;
